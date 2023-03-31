9c18cd87a1a831f49f18226d1d7df779c91e7204
Buse Nur <110244548+busenurcetin@users.noreply.github.com>
2023-03-31 10:18:34 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose"),
      passportLocalMongoose = require("passport-local-mongoose");
-
      const UserSchema = new mongoose.Schema({
         username: String,
         password: String
      });
-
      UserSchema.plugin(passportLocalMongoose);
-
      module.exports= mongoose.model("User", UserSchema);
