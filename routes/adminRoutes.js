296a82652ce5cd5a67a55248b1ae164e886544ab
Buse Nur <110244548+busenurcetin@users.noreply.github.com>
2023-03-31 10:19:52 +0300

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,6 @@ const express = require('express'),
 passport = require("passport"),
 User= require('../models/userModel'),
 router = express.Router();
-//videoda
 const app = express();
 
 //ARRAY
@@ -13,31 +12,26 @@ let adminActions =[
         actionId:1,
         actionName: "changeHomeImage",
         displayName: "Change Home Image"
-
     },
     {
         actionId:2,
         actionName: "changeAboutImage",
         displayName: "Change About Image"
-
     },
     {
         actionId:3,
         actionName: "changeAboutText",
         displayName: "Change About Text"
-
     },
     {
         actionId:4,
         actionName: "addNewBlog",
         displayName: "Add New Blog"
-
     },
     {
         actionId:5,
         actionName: "listAllBlogs",
         displayName: "List All Blogs"
-
     }
 ];
 
@@ -64,8 +58,6 @@ router.get("/admin", isLoggedIn, (req, res)=>{
  }) ;
 
  router.post("/signup", isLoggedIn, (req,res)=>{
-   
-
     let newUser = new User({username:req.body.username});
     User.register(newUser, req.body.password, (err, user)=>{
         if (err){
@@ -74,11 +66,9 @@ router.get("/admin", isLoggedIn, (req, res)=>{
         }
         passport.authenticate("local")(req, res, ()=>{
             res.redirect("/");
-
         });
     });
 }) ;
-
 
  //Sign Out
      
@@ -95,7 +85,6 @@ router.get("/admin", isLoggedIn, (req, res)=>{
       res.redirect('/');
     });
   });
-
  //Sign Out Bitiş
 
 function isLoggedIn(req, res, next)
@@ -105,6 +94,5 @@ function isLoggedIn(req, res, next)
     }
     res.redirect("/signin");
 }
-
 module.exports = router;
 
